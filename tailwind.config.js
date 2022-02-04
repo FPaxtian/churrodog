0084e73a90bff4c583fd5a35eff88dd381c266aa
Jesus <jesusmdz929@gmail.com>
2022-02-03 19:21:29 -0600

    add navbar && tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "rojito": "#ff141e",
+        "azulito":"#002360",
+        "grisesito":"#F6F6F6"
+      },
+    },
+  },
+  plugins: [],
+};
