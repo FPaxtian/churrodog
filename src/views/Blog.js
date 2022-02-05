5466c987004230cf34394220cc991abe821226ca
Jesus <jesusmdz929@gmail.com>
2022-02-05 00:24:27 -0600

    blog done

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Blog = () => {
+  return (
+    <div className="contenedor-padre bg-white w-auto h-[100vh]">
+      <div className="perro-cover w-auto h-[35rem] 2xl:w-[100%] bg-white "></div>
+      <hr className="salto" />
+      <div className="blog-contenedor w-auto h-auto bg-white ">
+        <h1 className=" px-20 py-10  text-xl lg:text-4xl 2xl:text-5xl 2xl:pl-44 text-left font-bold text-black">
+          1. Establece reglas claras desde el principio
+        </h1>
+        <br></br>
+        <h1 className=" pb-20 2xl:text-2xl 2xl:mt-0 px-20 2xl:px-44 text-slate-800">
+          Ser una empresa de clase mundial y abarcar la mayor parte del mercado
+          ofreciendo productos de alta digestibilidad sin aditivos ni
+          conservadores generando un desarrollo integral para nuestros
+          colaboradores y su entorno social.
+        </h1>
+      </div>
+      <hr className="salto" />
+      <div className="blog-contenedor w-auto h-auto bg-white ">
+        <h1 className=" p-10 text-xl lg:text-4xl 2xl:text-5xl 2xl:pl-44 text-left font-bold text-black">
+          1. Establece reglas claras desde el principio
+        </h1>
+        <br></br>
+        <h1 className=" pb-20 2xl:text-2xl 2xl:mt-0 2xl:px-44 text-slate-800">
+          Ser una empresa de clase mundial y abarcar la mayor parte del mercado
+          ofreciendo productos de alta digestibilidad sin aditivos ni
+          conservadores generando un desarrollo integral para nuestros
+          colaboradores y su entorno social.
+        </h1>
+      </div>
+      <hr className="salto" />
+       <div className="blog-contenedor w-auto h-auto bg-white ">
+        <h1 className=" p-10 text-xl lg:text-4xl 2xl:text-5xl 2xl:pl-44 text-left font-bold text-black">
+          1. Establece reglas claras desde el principio
+        </h1>
+        <br></br>
+        <h1 className=" pb-20 2xl:text-2xl 2xl:mt-0 2xl:px-44 text-slate-800">
+          Ser una empresa de clase mundial y abarcar la mayor parte del mercado
+          ofreciendo productos de alta digestibilidad sin aditivos ni
+          conservadores generando un desarrollo integral para nuestros
+          colaboradores y su entorno social.
+        </h1>
+      </div>
+      <hr className="salto" />
+    </div>
+  );
+};
 
-    return (
-        <h1>Blog</h1>
-    )
-}
-
-export default Blog 
+export default Blog;
