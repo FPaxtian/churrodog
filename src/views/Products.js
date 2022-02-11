915d570f9c5bb484422d970d7ec0c9cb0ab402d6
Francisco Paxtian <fizquierdo@quaxar.com>
2022-02-10 21:36:29 -0600

    products

---
+++ src/views/Products.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import Product from "../components/_product";
+import axios from "axios";
+
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const url = "https://churro-dog-server-test.herokuapp.com/"
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+
+
+  const getProducts = async () => {
+    try {
+      console.log(`${url}api/v1/products/`);
+      await axios
+        .get(`${url}api/v1/products/`)
+        .then((res) => {
+          setProducts(res.data);
+          setLoader(false);
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    } catch (error) { }
+  };
+
+  return (
+    <div className="contenedor padre bg-black w-auto h-auto">
+      <div className="w-full h-[auto] bg-white mx-auto">
+        <div className="p-10">
+          <h1 className="text-xl lg:text-4xl 2xl:text-5xl 2xl:pl-44 text-left font-bold text-black">
+            Nuestros p<a className="underline decoration-rojito">roductos</a>
+          </h1>
+        </div>
+        {loader && (
+          <div className="w-full flex justify-center items-center content-center">
+            <img
+              src={require("../assets/perroEsperando.gif")}
+              alt="Funny image"
+            />
+          </div>
+        )}
+        <div class="container mx-auto">
+          <div className="contenedor-padre w-full h-auto grid grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4">
+            {products.map((data) => {
+              return <Product key={data._id} product={data} />;
+            })}
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Products;
