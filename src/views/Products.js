54151d57ee6108aa48d93fa7e997b0e80e41398d
Francisco Paxtian <fizquierdo@quaxar.com>
2022-02-10 21:42:47 -0600

    products3

--- src/views/Products.js
+++ src/views/Products.js
@@ -28,31 +28,7 @@ const Products = () => {
   // };
 
   return (
-    <div className="contenedor padre bg-black w-auto h-auto">
-      <div className="w-full h-[auto] bg-white mx-auto">
-        <div className="p-10">
-          <h1 className="text-xl lg:text-4xl 2xl:text-5xl 2xl:pl-44 text-left font-bold text-black">
-            Nuestros p<a className="underline decoration-rojito">roductos</a>
-          </h1>
-        </div>
-        {loader && (
-          <div className="w-full flex justify-center items-center content-center">
-            <img
-              src={require("../assets/perroEsperando.gif")}
-              alt="Funny image"
-            />
-          </div>
-        )}
-        <div class="container mx-auto">
-          <div className="contenedor-padre w-full h-auto grid grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4">
-            {products.map((data) => {
-              return <Product key={data._id} product={data} />;
-            })}
-          </div>
-        </div>
-
-      </div>
-    </div>
+    <div>product</div>
   );
 };
 
